344451ad34575bed87556d938b2a77f00f1e9d2b
Philipe La Madeleine <philippe117@live.fr>
2020-12-09 10:38:16 -0500

    fix 1

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     py_modules=[package_name],
     entry_points={
         'console_scripts': [
-            'helloWorld = helloWorld:main'
+            'helloWorld = helloWorld'
         ]
     },
     license='License :: OSI Approved :: MIT License',
